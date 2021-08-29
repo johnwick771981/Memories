b13bd446461bb5d1e0392f514da1e15409ff79ee
unknown <johnwick771981@gmail.com>
2021-08-29 19:01:48 +0530

    Hello

--- server/index.js
+++ server/index.js
@@ -4,21 +4,22 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 import postRoutes from './routes/posts.js';
+ 
 
 const app = express();
-
-app.use('/posts' , postRoutes);
 
 app.use(bodyParser.json({limit : "30mb" , extended: true}));
 app.use(bodyParser.urlencoded({limit : "30mb" , extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://anirudh_025:Anirudh@007@cluster0.csodj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.use('/posts' , postRoutes);
 
-const PORT = process.env.PORT || 5000;
+ const CONNECTION_URL = 'mongodb+srv://anirudh_025:Anirudh@007@cluster0.csodj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(CONNECTION_URL , { useNewUrlParser : true , useUnifiedTopology : true})
-    .then(() => app.listen(PORT , () => console.log('Server Running on port : 5000')))
+    .then(() => app.listen(PORT , () => console.log('Server Running on port : 3001')))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify' , false);
